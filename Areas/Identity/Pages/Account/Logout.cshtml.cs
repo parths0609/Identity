50f84d9b0e0c093e1584c24018a2484a40b4870e
parth0609 <shekokarparth@gmail.com>
2020-05-28 16:32:55 +0530

    Signed-off-by: parth0609 <shekokarparth@gmail.com>

---
+++ Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace LoginIdentityDemo.Areas.Identity.Pages.Account
+{
+    [AllowAnonymous]
+    public class LogoutModel : PageModel
+    {
+        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly ILogger<LogoutModel> _logger;
+
+        public LogoutModel(SignInManager<IdentityUser> signInManager, ILogger<LogoutModel> logger)
+        {
+            _signInManager = signInManager;
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+
+        public async Task<IActionResult> OnPost(string returnUrl = null)
+        {
+            await _signInManager.SignOutAsync();
+
+            HttpContext.Session.Clear();
+            CookieOptions option = new CookieOptions();
+            if (Request.Cookies[".AspNetCore.Identity.Application"] != null)
+            {
+                option.Expires = DateTime.Now.AddDays(-1);
+                Response.Cookies.Append(".AspNetCore.Identity.Application", "", option);
+                //option.MaxAge = TimeSpan.FromDays(-1);
+
+            }
+
+            _logger.LogInformation("User logged out.");
+            if (returnUrl != null)
+            {
+                return LocalRedirect(returnUrl);
+            }
+            else
+            {
+                return RedirectToPage();
+            }
+        }
+    }
+}
